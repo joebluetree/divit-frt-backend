c850004bd4454783b6594630ad130355d6c53495
alen.c <alen.c@divitsoftlabs.com>
2025-01-20 11:38:41 +0530

    table create

---
+++ db/Database.Models/Masters/mast_mail_server.cs
@@ -0,0 +1,20 @@
+using System;
+using Database.Models.BaseTables;
+
+namespace Database.Models.Masters;
+
+public class mast_mail_server : baseTable_company
+{
+    public int mail_id { get; set; }
+    public string? mail_name { get; set; }
+    public string? mail_smtp_name { get; set; }
+    public string? mail_smtp_port { get; set; }
+    public string? mail_is_ssl { get; set; }
+    public string? mail_is_auth { get; set; }
+    public string? mail_is_spa { get; set; }
+    public int? mail_bulk_tot { get; set; }
+    public int? mail_bulk_sub { get; set; }
+    public string? mail_smtp_username { get; set; }
+    public string? mail_smtp_pwd { get; set; }
+    
+}
