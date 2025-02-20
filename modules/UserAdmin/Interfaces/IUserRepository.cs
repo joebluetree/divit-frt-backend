bffc139e96893515abf94171102de2468fb32170
sourav.v <sourav.v@divitsoftlabs.com>
2025-02-20 12:06:07 +0530

    userm corrected

--- modules/UserAdmin/Interfaces/IUserRepository.cs
+++ modules/UserAdmin/Interfaces/IUserRepository.cs
@@ -7,7 +7,7 @@ namespace UserAdmin.Interfaces
     {
         Task<Dictionary<string, object>> GetListAsync(Dictionary<string, object> data);
         Task<mast_userm_dto?> GetRecordAsync(int comp_id, int id);
-        Task<List<mast_userbranches_dto>> GetUserBranchesAsync(int comp_id, int id);
+        Task<List<mast_userbranches_dto>> GetuserBranchesAsync(int comp_id, int id);
         Task<mast_userm_dto> SaveAsync(int id, string mode, mast_userm_dto record);
         Task<mast_userm_dto> SaveParentAsync(int id, string mode, mast_userm_dto record);
         Task<Dictionary<string, object>> DeleteAsync(int id);
