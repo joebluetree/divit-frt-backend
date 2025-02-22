08da0edea94cd7b7ac23fdbf34c3ab9e7d37c76e
alen.c <alen.c@divitsoftlabs.com>
2025-02-22 09:53:08 +0530

    scripts added3

--- db/Database.Models/Cargo/cargo_housem.cs
+++ db/Database.Models/Cargo/cargo_housem.cs
@@ -256,8 +256,8 @@ public class cargo_housem
     public DateTime rec_created_date { get; set; }
     public string? rec_edited_by { get; set; }
     public DateTime? rec_edited_date { get; set; }
-    public int? rec_company_id { get; set; }
-    public int? rec_branch_id { get; set; }
+    public int rec_company_id { get; set; }
+    public int rec_branch_id { get; set; }
 
 
     [ForeignKey("rec_company_id")]
--- db/Database.Models/Cargo/cargo_masterm.cs
+++ db/Database.Models/Cargo/cargo_masterm.cs
@@ -190,8 +190,8 @@ public class cargo_masterm
     public DateTime rec_created_date { get; set; }
     public string? rec_edited_by { get; set; }
     public DateTime? rec_edited_date { get; set; }
-    public int? rec_company_id { get; set; }
-    public int? rec_branch_id { get; set; }
+    public int rec_company_id { get; set; }
+    public int rec_branch_id { get; set; }
 
     [ForeignKey("rec_company_id")]
     public mast_companym? company { get; set; }
