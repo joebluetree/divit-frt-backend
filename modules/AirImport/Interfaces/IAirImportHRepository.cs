30709e816880385cf37592e7ae2a98c0f96c8d44
alen.c <alen.c@divitsoftlabs.com>
2025-04-16 15:24:26 +0530

    Added followup into  CommonShipment

--- modules/AirImport/Interfaces/IAirImportHRepository.cs
+++ modules/AirImport/Interfaces/IAirImportHRepository.cs
@@ -12,7 +12,7 @@ namespace AirImport.Interfaces
     {
         Task<Dictionary<string, object>> GetListAsync(Dictionary<string, object> data);
         Task<cargo_air_importh_dto?> GetRecordAsync(int id);
-       Task<cargo_air_importh_dto?> GetDefaultDataAsync(int id);
+        Task<cargo_air_importh_dto?> GetDefaultDataAsync(int id);
         Task<cargo_air_importh_dto> SaveAsync(int id, string mode, cargo_air_importh_dto record);
         Task<cargo_air_importh_dto> SaveParentAsync(int id, string mode, cargo_air_importh_dto record);
         Task<Dictionary<string, object>> DeleteAsync(int id);
---
+++ modules/CommonShipment/Interfaces/IFollowUpRepository.cs
@@ -0,0 +1,19 @@
+using Common.DTO.CommonShipment;
+
+namespace CommonShipment.Interfaces
+{
+    //Name : Alen Cherian
+    //Date : 09/04/2025
+    //Remark : Version 1.0
+
+    public interface IFollowUpRepository
+    {
+        Task<Dictionary<string, object>> GetListAsync(Dictionary<string, object> data);
+        Task<cargo_followup_dto?> GetRecordAsync(int id);
+        Task<cargo_followup_dto?> GetDefaultDataAsync(int id);
+        Task<List<cargo_followup_dto>> GetDetailsAsync(int id);
+        Task<cargo_followup_dto> SaveAsync(int id, string mode, cargo_followup_dto record);
+        Task<cargo_followup_dto> SaveParentAsync(int id, string mode, cargo_followup_dto record);
+        Task<Dictionary<string, object>> DeleteAsync(int id);
+    }
+}
